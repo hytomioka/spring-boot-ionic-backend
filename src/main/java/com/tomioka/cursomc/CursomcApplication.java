4ab8103f440565448134f05a8ea29f16dee0668c
HY Tomioka <henriqueyudi2112@gmail.com>
2021-01-13 17:35:52 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/tomioka/cursomc/CursomcApplication.java
+++ src/main/java/com/tomioka/cursomc/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.tomioka.cursomc.domain.Categoria;
+import com.tomioka.cursomc.domain.Produto;
 import com.tomioka.cursomc.repositories.CategoriaRepository;
+import com.tomioka.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -16,16 +18,31 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
---
+++ src/main/java/com/tomioka/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.tomioka.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tomioka.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+	
+}
