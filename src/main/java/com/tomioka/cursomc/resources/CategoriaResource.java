ebc1b49c3642723e5b20c6d633d12990e23d8136
HY Tomioka <henriqueyudi2112@gmail.com>
2021-01-13 18:11:43 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/tomioka/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tomioka/cursomc/resources/CategoriaResource.java
@@ -23,5 +23,4 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
 }
--- src/main/java/com/tomioka/cursomc/services/CategoriaService.java
+++ src/main/java/com/tomioka/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.tomioka.cursomc.domain.Categoria;
 import com.tomioka.cursomc.repositories.CategoriaRepository;
+import com.tomioka.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado. Id= " + id + ", Tipo= " + Categoria.class.getName()));
 	}
 
 }
