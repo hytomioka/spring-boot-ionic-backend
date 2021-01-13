7ca3fc67d26f18dd6172fb11df98ba331a43674c
HY Tomioka <henriqueyudi2112@gmail.com>
2021-01-13 15:41:49 -0300

    Criando operacao de instanciacao

--- src/main/java/com/tomioka/cursomc/CursomcApplication.java
+++ src/main/java/com/tomioka/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.tomioka.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.tomioka.cursomc.domain.Categoria;
+import com.tomioka.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
