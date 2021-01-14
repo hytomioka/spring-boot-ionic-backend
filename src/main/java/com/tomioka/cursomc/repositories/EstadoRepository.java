7d5fd2bfd3a1496b4260cd8d0dd4816bead607d9
HY Tomioka <henriqueyudi2112@gmail.com>
2021-01-13 22:44:48 -0300

    Cidade e Estado

---
+++ src/main/java/com/tomioka/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.tomioka.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tomioka.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+}
