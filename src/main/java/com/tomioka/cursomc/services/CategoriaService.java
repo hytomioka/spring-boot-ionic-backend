bad6733a2f68922783c5cd039de3b0f3e0cd87eb
HY Tomioka <henriqueyudi2112@gmail.com>
2021-01-20 12:55:12 -0300

    Deletando uma categoria com o DELETE

--- src/main/java/com/tomioka/cursomc/services/CategoriaService.java
+++ src/main/java/com/tomioka/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.tomioka.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.tomioka.cursomc.domain.Categoria;
 import com.tomioka.cursomc.repositories.CategoriaRepository;
+import com.tomioka.cursomc.services.exceptions.DataIntegrityException;
 import com.tomioka.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj); // spring possui o mesmo método para guardar ou atualizar os dados
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos");
+		}
 	}
 
 }
