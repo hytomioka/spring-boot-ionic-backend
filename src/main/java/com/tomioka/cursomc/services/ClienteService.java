fcd8833061e61e0922d29aa002b650e25dba281b
HY Tomioka <henriqueyudi2112@gmail.com>
2021-01-20 23:14:38 -0300

    Inserindo novo Cliente com POST

--- src/main/java/com/tomioka/cursomc/services/ClienteService.java
+++ src/main/java/com/tomioka/cursomc/services/ClienteService.java
@@ -9,25 +9,42 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tomioka.cursomc.domain.Cidade;
 import com.tomioka.cursomc.domain.Cliente;
+import com.tomioka.cursomc.domain.Endereco;
+import com.tomioka.cursomc.domain.enums.TipoCliente;
 import com.tomioka.cursomc.dto.ClienteDTO;
+import com.tomioka.cursomc.dto.ClienteNewDTO;
 import com.tomioka.cursomc.repositories.ClienteRepository;
+import com.tomioka.cursomc.repositories.EnderecoRepository;
 import com.tomioka.cursomc.services.exceptions.DataIntegrityException;
 import com.tomioka.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado. Id= " + id + ", Tipo= " + Cliente.class.getName()));
 	}
-	
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEndereco());
+		return obj;
+	}
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
@@ -53,13 +70,30 @@ public class ClienteService {
 		return repo.findAll(pageRequest);
 
 	}
-	
+
 	// método para conversão de um objeto ClienteDTO para um objeto Cliente
-	// instancia uma nova Cliente a partir de um DTO
+	// instancia um novo objeto Cliente a partir de um DTO
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEndereco().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
